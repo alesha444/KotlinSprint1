d60c183950b797cce20bb9984f5aaf72301ea2cd
liss759 <alesha444@gmail.com>
2024-10-12 14:01:37 +0300

    KS-1-1 create smth

--- src/main/kotlin/lesson1/lesson1_task1.kt
+++ src/main/kotlin/lesson1/lesson1_task1.kt
@@ -2,5 +2,5 @@ package org.example.lesson1
 
 fun main() {
     var numberOfOrders = 0
-    val thankMessage = "Спасибо за бриобретение высококачественных товаров в нашей компании!"
+    val thankMessage = "Спасибо за приобретение высококачественных товаров в нашей компании!"
 }
