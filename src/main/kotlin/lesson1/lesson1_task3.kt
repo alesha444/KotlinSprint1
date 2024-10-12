2d64abb9456af8415e78465c8b2a5cea99fd55dd
liss759 <alesha444@gmail.com>
2024-10-12 20:00:37 +0300

    KS-1-3 polet Gagarina

---
+++ src/main/kotlin/lesson1/lesson1_task3.kt
@@ -0,0 +1,13 @@
+package org.example.lesson1
+
+fun main(){
+    val year = 1961
+    var hour = 9
+    var minute = 7
+
+    println("Год полёта - ${year}")
+    println("Взлёт - ${0}${+hour}:${0}${+minute}")
+    hour=10
+    minute=55
+    println("Посадка - ${+hour}:${+minute}")
+}
