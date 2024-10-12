ae8ab29e090eb3d5a385385ccca6567a1158b992
liss759 <alesha444@gmail.com>
2024-10-12 14:29:19 +0300

    KS-1-2 create employees

---
+++ src/main/kotlin/lesson1/lesson1_task2.kt
@@ -0,0 +1,14 @@
+package org.example.lesson1
+
+fun main() {
+    var numberOfOrders = 75
+    val notify = "Ваш заказ № "
+    val thankMessage = "Спасибо за приобретение высококачественных товаров в нашей компании!"
+
+    println("${notify}${numberOfOrders} \n${thankMessage}")
+
+    var employees = 2000
+    println("Всего в компании работает: ${employees}")
+
+
+}
